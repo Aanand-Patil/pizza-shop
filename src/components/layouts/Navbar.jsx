a265c1dce62ce7dcbd756d388a08a3ab54713ab1
Aanand-Patil <anandpatil6440@gmail.com>
2025-03-21 12:28:17 +0530

    order,login,signup page

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -68,11 +68,11 @@ function Navbar() {
               fill='none'
               viewBox='0 0 24 24'
               stroke='currentColor'
-              stroke-width='1.5'
+              strokeWidth='1.5'
             >
               <path
-                stroke-linecap='round'
-                stroke-linejoin='round'
+                strokeLinecap='round'
+                strokeLinejoin='round'
                 d='M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1'
               />
             </svg>
